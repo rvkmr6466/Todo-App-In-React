5cd605aef33ef804c1f0a9dcd21b20c95349957e
Ravi Kumar <ravi.kumar@flexiele.com>
2021-07-12 02:02:19 +0530

    All CSS done for todo list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import Todo from './Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        To Do List
-      </header>
-    </div>
-  );
+
+	const DateObject = new Date();
+	const curYear = DateObject.getFullYear();
+	// console.log(curYear);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				To Do List
+			</header>
+
+			<Todo />
+
+			<footer className="App-footer">
+				Copyright &#169; <span id="year">{curYear}</span>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function Todo() {
+    return (
+        <div className="todo-container">
+            <header className="todo-header">Todo List</header>
+            <div className="field-container">
+                <input type="text" />
+                <button>+</button>
+            </div>
+            <div className="items-container">
+                <ul>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                    <li>Buy Milk</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
